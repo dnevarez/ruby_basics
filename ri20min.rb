0fad59686927cc2b838427d713ebab91e8a5ee1f
Dan Nevarez <dnev.dev@gmail.com>
2016-11-30 22:02:21 -0700

    ri20min

--- ri20min.rb
+++ ri20min.rb
@@ -10,14 +10,14 @@ class MegaGreeter
 
   # Say hi to everybody
   def say_hi
-    if @names.nil?
+    if @names.nil? # if names is nil, print 3 dots
       puts "..."
-    elsif @names.respond_to?("each")
+    elsif @names.respond_to?("each")  # if names responds to "each", it means it's something that can be iterated over.
       # @names is a list of some kind, iterate!
-      @names.each do |name|
-        puts "Hello #{name}!"
+      @names.each do |name|           # each is a method that accepts a block of code then runs that block of code for every element in a list, and the bit between do and end is just such a block. A block is like an anonymous function or lambda. The variable between pipe characters is the parameter for this block.
+        puts "Hello #{name}!"         # This is similar to a for loop without the necessity of an extra variable for the iterator.
       end
-    else
+    else                              # otherwise just print name as part of the string.
       puts "Hello #{@names}!"
     end
   end
