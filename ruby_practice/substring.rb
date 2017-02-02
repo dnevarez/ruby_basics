4446b7e83c6fcb3fef86db6403cccc7a67c6e312
Dan Nevarez <dnev.dev@gmail.com>
2017-02-02 15:50:03 -0700

    comments and refactoring

--- ruby_practice/substring.rb
+++ ruby_practice/substring.rb
@@ -8,6 +8,8 @@ dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","o
 # This was my original solution, but I immediately felt there was a much better way,
 # without using to each methods on two seperate arrays.
 
+=begin
+
 def substring(str, dictionary)
   words_found = Hash.new(0)
   string_array = str.split(' ')
@@ -22,10 +24,14 @@ def substring(str, dictionary)
   words_found
 end
 
+=end
+
 # That's when I stumbled on scan. Scan will iterate through a string and return
 # an array of all the values matching it's argument (which can be either a String or Regex)
 # Then we simply call the count method on the array and assign the value to counter, which
 # if greater than zero will be assigned to a hash key matching the value.
+
+=begin
 
 def substring(str, dictionary)
   words_found = Hash.new(0)
@@ -34,6 +40,26 @@ def substring(str, dictionary)
     counter = str.downcase.scan(word).count
     words_found[word] = counter if counter > 0
   end
-  
+
+  words_found
+end
+
+=end
+
+# With some further reading, into count vs length vs size. It would seem that best
+# method to call in this case would be either length or size, depending on preference.
+# The reason being is that count is now enumerating through the array we've created. But since
+# we are not passing it an argument, we don't need to enumerate through the array. We just the length.
+
+# So we change count to either length or size.
+
+def substring(str, dictionary)
+  words_found = Hash.new(0)
+
+  dictionary.each do |word|
+    counter = str.downcase.scan(word).size
+    words_found[word] = counter if counter > 0
+  end
+
   words_found
 end
